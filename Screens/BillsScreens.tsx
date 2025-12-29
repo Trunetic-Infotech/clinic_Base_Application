3331d0a262ad374a44ad5cff19d9d5a24e9d9f2c
sahil <Sahil.s@truneticinfotech.com>
2025-12-29 17:45:19 +0530

    Add gradient bg

--- Screens/BillsScreens.tsx
+++ Screens/BillsScreens.tsx
@@ -1,10 +1,18 @@
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function BillsScreens() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold">Bills Screen</Text>
-    </SafeAreaView>
+    <LinearGradient
+      colors={['rgba(162, 236, 255, 0.89)', '#FFFFFF']}
+      locations={[0.03, 0.55]}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 1 }}
+      style={{ flex: 1 }}>
+      <SafeAreaView className="flex-1 items-center justify-center">
+        <Text className="text-xl font-bold">Bills Screen</Text>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
